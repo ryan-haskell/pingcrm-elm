38f6fecf13d434ec49cc1759e03b128a11377520
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2024-06-03 14:46:05 -0500

    Update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
-import vue from '@vitejs/plugin-vue'
 import elm from 'vite-plugin-elm-watch'
 
+const isInDevelopment = process.env.NODE_ENV === 'development'
+
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/elm/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-        elm({ mode: 'standard' })
-    ],
+  plugins: [
+    laravel({
+      input: 'resources/elm/app.js',
+      refresh: true,
+    }),
+    elm({ mode: isInDevelopment ? 'standard' : 'minify' })
+  ],
 })
