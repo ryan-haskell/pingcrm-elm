c164f54b0c46e7ffd8c46b850d0273deb3f61771
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2024-06-04 17:22:32 -0500

    Extract out Table component

---
+++ resources/elm/src/Components/Table.elm
@@ -0,0 +1,235 @@
+module Components.Table exposing
+    ( Props, view
+    , Column
+    )
+
+{-|
+
+@docs Props, view
+
+@docs Column
+
+-}
+
+import Components.Icon
+import Context exposing (Context)
+import Extra.Url
+import Html exposing (..)
+import Html.Attributes as Attr exposing (class, href, style)
+
+
+
+-- TABLE
+
+
+type alias Props data =
+    { context : Context
+    , name : String
+    , baseUrl : String
+    , toId : data -> Int
+    , columns : List (Column data)
+    , rows : List data
+    , lastPage : Int
+    }
+
+
+type alias Column data =
+    { name : String, toValue : data -> String }
+
+
+view : Props data -> Html msg
+view props =
+    div []
+        [ viewTableFilters props
+        , viewTableData props
+        , viewTableFooter props
+        ]
+
+
+viewTableFilters : Props data -> Html msg
+viewTableFilters props =
+    div [ class "flex items-center justify-between mb-6" ]
+        [ div [ class "flex items-center mr-4 w-full max-w-md" ]
+            [ div [ class "flex w-full bg-white rounded shadow" ]
+                [ button
+                    [ Attr.type_ "button"
+                    , class "focus:z-10 px-4 hover:bg-gray-100 border-r focus:border-white rounded-l focus:ring md:px-6"
+                    ]
+                    [ div [ class "flex items-baseline" ]
+                        [ span [ class "hidden text-gray-700 md:inline" ]
+                            [ text "Filter" ]
+                        , Components.Icon.arrowDown
+                        ]
+                    ]
+                , input
+                    [ class "relative px-6 py-3 w-full rounded-r focus:shadow-outline"
+                    , Attr.autocomplete False
+                    , Attr.type_ "text"
+                    , Attr.name "search"
+                    , Attr.placeholder "Search…"
+                    ]
+                    []
+                ]
+            , button
+                [ class "ml-3 text-gray-500 hover:text-gray-700 focus:text-indigo-500 text-sm"
+                , Attr.type_ "button"
+                ]
+                [ text "Reset" ]
+            ]
+        , a [ class "btn-indigo", href (props.baseUrl ++ "/create") ]
+            [ span [] [ text "Create" ]
+            , text " "
+            , span [ class "hidden md:inline" ] [ text props.name ]
+            ]
+        ]
+
+
+viewTableData : Props data -> Html msg
+viewTableData props =
+    div [ class "bg-white rounded-md shadow overflow-x-auto" ]
+        [ table
+            [ class "w-full whitespace-nowrap" ]
+            [ thead [] [ viewTableHeaderRow props ]
+            , tbody [] (List.map (viewTableBodyRow props) props.rows)
+            ]
+        ]
+
+
+viewTableHeaderRow : Props data -> Html msg
+viewTableHeaderRow props =
+    let
+        lastColumnIndex : Int
+        lastColumnIndex =
+            List.length props.columns - 1
+
+        viewCell : Int -> String -> Html msg
+        viewCell index name =
+            th
+                [ class "pb-4 pt-6 px-6"
+                , Attr.colspan
+                    (if index == lastColumnIndex then
+                        2
+
+                     else
+                        1
+                    )
+                ]
+                [ text name ]
+    in
+    tr [ class "text-left font-bold" ]
+        (List.indexedMap viewCell (List.map .name props.columns))
+
+
+viewTableBodyRow : Props data -> data -> Html msg
+viewTableBodyRow props data =
+    let
+        editUrl : String
+        editUrl =
+            props.baseUrl ++ "/" ++ String.fromInt (props.toId data) ++ "/edit"
+
+        viewCell : Int -> (data -> String) -> Html msg
+        viewCell index toValue =
+            if index == 0 then
+                viewFirstCell toValue
+
+            else
+                viewOtherCell toValue
+
+        viewFirstCell : (data -> String) -> Html msg
+        viewFirstCell toValue =
+            td [ class "border-t" ]
+                [ a
+                    [ class "flex items-center px-6 py-4 focus:text-indigo-500"
+                    , href editUrl
+                    ]
+                    [ text (toValue data) ]
+                ]
+
+        viewOtherCell : (data -> String) -> Html msg
+        viewOtherCell toValue =
+            td [ class "border-t" ]
+                [ a
+                    [ class "flex items-center px-6 py-4"
+                    , Attr.tabindex -1
+                    , href editUrl
+                    ]
+                    [ text (toValue data) ]
+                ]
+    in
+    tr [ class "hover:bg-gray-100 focus-within:bg-gray-100" ]
+        ((props.columns
+            |> List.map .toValue
+            |> List.indexedMap viewCell
+         )
+            ++ [ td [ class "w-px border-t" ]
+                    [ a
+                        [ class "flex items-center px-4"
+                        , Attr.tabindex -1
+                        , href editUrl
+                        ]
+                        [ Components.Icon.chevronRight ]
+                    ]
+               ]
+        )
+
+
+viewTableFooter : Props data -> Html msg
+viewTableFooter props =
+    let
+        currentPage : Int
+        currentPage =
+            Extra.Url.getQueryParameter "page" props.context.url
+                |> Maybe.andThen String.toInt
+                |> Maybe.withDefault 1
+
+        viewPreviousLink : Html msg
+        viewPreviousLink =
+            viewLink
+                { label = "« Previous"
+                , isDisabled = currentPage == 1
+                , url = props.baseUrl ++ "?page=" ++ String.fromInt (currentPage - 1)
+                }
+
+        viewNextLink : Html msg
+        viewNextLink =
+            viewLink
+                { label = "Next »"
+                , isDisabled = currentPage == props.lastPage
+                , url = props.baseUrl ++ "?page=" ++ String.fromInt (currentPage + 1)
+                }
+
+        viewPageNumberLink : Int -> Html msg
+        viewPageNumberLink num =
+            a
+                [ class "mb-1 mr-1 px-4 py-3 focus:text-indigo-500 text-sm leading-4 hover:bg-white border focus:border-indigo-500 rounded"
+                , Attr.classList [ ( "bg-white", currentPage == num ) ]
+                , href (props.baseUrl ++ "?page=" ++ String.fromInt num)
+                ]
+                [ text (String.fromInt num) ]
+
+        viewLink :
+            { label : String
+            , isDisabled : Bool
+            , url : String
+            }
+            -> Html msg
+        viewLink link =
+            if link.isDisabled then
+                div [ class "mb-1 mr-1 px-4 py-3 text-gray-400 text-sm leading-4 border rounded" ] [ text link.label ]
+
+            else
+                a
+                    [ class "mb-1 mr-1 px-4 py-3 focus:text-indigo-500 text-sm leading-4 hover:bg-white border focus:border-indigo-500 rounded"
+                    , href link.url
+                    ]
+                    [ text link.label ]
+    in
+    div [ class "mt-6" ]
+        [ div [ class "flex flex-wrap -mb-1" ]
+            (List.concat
+                [ [ viewPreviousLink ]
+                , List.range 1 10 |> List.map viewPageNumberLink
+                , [ viewNextLink ]
+                ]
+            )
+        ]
--- resources/elm/src/Pages/Organizations.elm
+++ resources/elm/src/Pages/Organizations.elm
@@ -14,6 +14,7 @@ module Pages.Organizations exposing
 
 import Browser exposing (Document)
 import Components.Icon
+import Components.Table
 import Context exposing (Context)
 import Domain.Auth exposing (Auth)
 import Domain.Flash exposing (Flash)
@@ -101,182 +102,22 @@ view ctx model =
         , user = model.props.auth.user
         , content =
             [ h1 [ class "mb-8 text-3xl font-bold" ] [ text "Organizations" ]
-            , viewTableFilters model
-            , viewTable model
-            , viewTableFooter ctx model
+            , Components.Table.view
+                { context = ctx
+                , name = "Organization"
+                , baseUrl = "/organizations"
+                , toId = .id
+                , columns = columns
+                , rows = model.props.organizations
+                , lastPage = model.props.lastPage
+                }
             ]
         }
 
 
-viewTableFilters : Model -> Html Msg
-viewTableFilters model =
-    div
-        [ class "flex items-center justify-between mb-6" ]
-        [ div
-            [ class "flex items-center mr-4 w-full max-w-md" ]
-            [ div
-                [ class "flex w-full bg-white rounded shadow" ]
-                [ button
-                    [ Attr.type_ "button"
-                    , class
-                        "focus:z-10 px-4 hover:bg-gray-100 border-r focus:border-white rounded-l focus:ring md:px-6"
-                    ]
-                    [ div
-                        [ class "flex items-baseline" ]
-                        [ span
-                            [ class "hidden text-gray-700 md:inline" ]
-                            [ text "Filter" ]
-                        , Components.Icon.arrowDown
-                        ]
-                    ]
-                , input
-                    [ class
-                        "relative px-6 py-3 w-full rounded-r focus:shadow-outline"
-                    , Attr.autocomplete False
-                    , Attr.type_ "text"
-                    , Attr.name "search"
-                    , Attr.placeholder "Search…"
-                    ]
-                    []
-                ]
-            , button
-                [ class
-                    "ml-3 text-gray-500 hover:text-gray-700 focus:text-indigo-500 text-sm"
-                , Attr.type_ "button"
-                ]
-                [ text "Reset" ]
-            ]
-        , a
-            [ class "btn-indigo", href "/organizations/create" ]
-            [ span [] [ text "Create" ]
-            , text " "
-            , span [ class "hidden md:inline" ] [ text "Organization" ]
-            ]
-        ]
-
-
-viewTable : Model -> Html Msg
-viewTable model =
-    div
-        [ class "bg-white rounded-md shadow overflow-x-auto" ]
-        [ table
-            [ class "w-full whitespace-nowrap" ]
-            [ thead []
-                [ tr
-                    [ class "text-left font-bold" ]
-                    [ th [ class "pb-4 pt-6 px-6" ] [ text "Name" ]
-                    , th [ class "pb-4 pt-6 px-6" ] [ text "City" ]
-                    , th [ class "pb-4 pt-6 px-6", Attr.colspan 2 ] [ text "Phone" ]
-                    ]
-                ]
-            , tbody [] (List.map viewTableRow model.props.organizations)
-            ]
-        ]
-
-
-viewTableRow : Organization -> Html Msg
-viewTableRow org =
-    let
-        editUrl : String
-        editUrl =
-            "/organizations/" ++ String.fromInt org.id ++ "/edit"
-    in
-    tr [ class "hover:bg-gray-100 focus-within:bg-gray-100" ]
-        [ td [ class "border-t" ]
-            [ a
-                [ class "flex items-center px-6 py-4 focus:text-indigo-500"
-                , href editUrl
-                ]
-                [ text org.name ]
-            ]
-        , td [ class "border-t" ]
-            [ a
-                [ class "flex items-center px-6 py-4"
-                , Attr.tabindex -1
-                , href editUrl
-                ]
-                [ text (org.city |> Maybe.withDefault "") ]
-            ]
-        , td [ class "border-t" ]
-            [ a
-                [ class "flex items-center px-6 py-4"
-                , Attr.tabindex -1
-                , href editUrl
-                ]
-                [ text (org.phone |> Maybe.withDefault "") ]
-            ]
-        , td [ class "w-px border-t" ]
-            [ a
-                [ class "flex items-center px-4"
-                , Attr.tabindex -1
-                , href editUrl
-                ]
-                [ Components.Icon.chevronRight ]
-            ]
-        ]
-
-
-viewTableFooter : Context -> Model -> Html Msg
-viewTableFooter { url } model =
-    let
-        currentPage : Int
-        currentPage =
-            Extra.Url.getQueryParameter "page" url
-                |> Maybe.andThen String.toInt
-                |> Maybe.withDefault 1
-
-        viewPreviousLink : Html Msg
-        viewPreviousLink =
-            viewLink
-                { label = "« Previous"
-                , isDisabled = currentPage == 1
-                , url = "/organizations?page=" ++ String.fromInt (currentPage - 1)
-                }
-
-        viewNextLink : Html Msg
-        viewNextLink =
-            viewLink
-                { label = "Next »"
-                , isDisabled = currentPage == model.props.lastPage
-                , url = "/organizations?page=" ++ String.fromInt (currentPage + 1)
-                }
-
-        viewPageNumberLink : String -> Html Msg
-        viewPageNumberLink num =
-            a
-                [ class "mb-1 mr-1 px-4 py-3 focus:text-indigo-500 text-sm leading-4 hover:bg-white border focus:border-indigo-500 rounded"
-                , Attr.classList
-                    [ ( "bg-white", String.fromInt currentPage == num )
-                    ]
-                , href ("/organizations?page=" ++ num)
-                ]
-                [ text num ]
-
-        viewLink : { label : String, isDisabled : Bool, url : String } -> Html Msg
-        viewLink props =
-            if props.isDisabled then
-                div [ class "mb-1 mr-1 px-4 py-3 text-gray-400 text-sm leading-4 border rounded" ] [ text props.label ]
-
-            else
-                a
-                    [ class "mb-1 mr-1 px-4 py-3 focus:text-indigo-500 text-sm leading-4 hover:bg-white border focus:border-indigo-500 rounded"
-                    , href props.url
-                    ]
-                    [ text props.label ]
-    in
-    div [ class "mt-6" ]
-        [ div [ class "flex flex-wrap -mb-1" ]
-            [ viewPreviousLink
-            , viewPageNumberLink "1"
-            , viewPageNumberLink "2"
-            , viewPageNumberLink "3"
-            , viewPageNumberLink "4"
-            , viewPageNumberLink "5"
-            , viewPageNumberLink "6"
-            , viewPageNumberLink "7"
-            , viewPageNumberLink "8"
-            , viewPageNumberLink "9"
-            , viewPageNumberLink "10"
-            , viewNextLink
-            ]
-        ]
+columns : List (Components.Table.Column Organization)
+columns =
+    [ { name = "Name", toValue = .name }
+    , { name = "City", toValue = .city >> Maybe.withDefault "" }
+    , { name = "Phone", toValue = .phone >> Maybe.withDefault "" }
+    ]
